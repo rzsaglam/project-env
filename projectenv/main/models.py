8ee8b52d9b049f414f1b64e6923205043c141226
rzsaglam <rzn.saglam@gmail.com>
2021-06-07 02:09:34 +0300

    Products

--- projectenv/main/models.py
+++ projectenv/main/models.py
@@ -8,7 +8,15 @@ class Paint(models.Model):
     type = models.CharField(max_length=200, default="none")
     color = models.CharField(max_length=200)
     stock = models.IntegerField(default=0)
-    weight = models.CharField(max_length=200)
+    KG20 = "20 KG"
+    KG12 = "12 KG"
+    KG5 = "5 KG"
+    WEIGHTCHOISES = [
+        (KG20, '20 KG'),
+        (KG12, '12 KG'),
+        (KG5, '5 KG'),
+    ]
+    weight = models.CharField(max_length=200, choices=WEIGHTCHOISES)
     IST = "İstanbul"
     YLV = "Yalova"
     LOCATIONCHOISES = [
@@ -16,7 +24,15 @@ class Paint(models.Model):
         (YLV, 'Yalova'),
     ]
     location = models.CharField(
-        max_length=200, choices=LOCATIONCHOISES, default=IST)
+        max_length=200, choices=LOCATIONCHOISES,)
+    BOYA = "Boya"
+    TINER = "Tiner"
+    TYPECHOISES = [
+        (BOYA, 'Boya'),
+        (TINER, 'Tiner')
+    ]
+    choice = models.CharField(
+        max_length=200, choices=TYPECHOISES,)
 
     def __str__(self):
         return self.name+' '+self.type+' '+self.color
--- projectenv/main/views.py
+++ projectenv/main/views.py
@@ -12,55 +12,277 @@ from django.contrib.auth import login, authenticate, logout
 
 
 def index(request):
-    paints = Paint.objects.all()
-    context = {
-        'paints': paints,
-    }
-    return render(request, 'pages/index.html', context,)
+    return render(request, 'pages/index.html')
 
 
 def big_paints_istanbul(request):
-    paints = Paint.objects.filter(location="İstanbul", weight="20 KG")
+    alkid = Paint.objects.filter(
+        name="Alkid", location="İstanbul", weight="20 KG", choice="Boya")
+    ferropox = Paint.objects.filter(
+        name="Ferropox", location="İstanbul", weight="20 KG", choice="Boya")
+    promega = Paint.objects.filter(
+        name="Promega", location="İstanbul", weight="20 KG", choice="Boya")
+    ferromel = Paint.objects.filter(
+        name="Ferromel", location="İstanbul", weight="20 KG", choice="Boya")
+    epoxy = Paint.objects.filter(
+        name="Epoxy", location="İstanbul", weight="20 KG", choice="Boya")
+    akrilik = Paint.objects.filter(
+        name="Akrilik", location="İstanbul", weight="20 KG", choice="Boya")
+    promarin = Paint.objects.filter(
+        name="Promarin", location="İstanbul", weight="20 KG", choice="Boya")
+    underwater = Paint.objects.filter(
+        name="Under Water", location="İstanbul", weight="20 KG", choice="Boya")
+    ferforje = Paint.objects.filter(
+        name="Ferforje", location="İstanbul", weight="20 KG", choice="Boya")
+    antipas = Paint.objects.filter(
+        name="Anti Pas", location="İstanbul", weight="20 KG", choice="Boya")
+    aluminyum = Paint.objects.filter(
+        name="Aluminyum", location="İstanbul", weight="20 KG", choice="Boya")
+    resistant = Paint.objects.filter(
+        name="Isıya Dayanıklı", location="İstanbul", weight="20 KG", choice="Boya")
     context = {
-        'paints': paints,
+        'alkid': alkid,
+        'ferropox': ferropox,
+        'promega': promega,
+        'ferromel': ferromel,
+        'epoxy': epoxy,
+        'akrilik': akrilik,
+        'promarin': promarin,
+        'underwater': underwater,
+        'ferforje': ferforje,
+        'antipas': antipas,
+        'aluminyum': aluminyum,
+        'resistant': resistant,
     }
     return render(request, 'pages/buyuk-boyalar-istanbul.html', context)
 
 
 def small_paints_istanbul(request):
-    paints = Paint.objects.filter(location="İstanbul", weight="5 KG")
+    alkid = Paint.objects.filter(
+        name="Alkid", location="İstanbul", weight="5 KG", choice="Boya")
+    ferropox = Paint.objects.filter(
+        name="Ferropox", location="İstanbul", weight="5 KG", choice="Boya")
+    promega = Paint.objects.filter(
+        name="Promega", location="İstanbul", weight="5 KG", choice="Boya")
+    ferromel = Paint.objects.filter(
+        name="Ferromel", location="İstanbul", weight="5 KG", choice="Boya")
+    epoxy = Paint.objects.filter(
+        name="Epoxy", location="İstanbul", weight="5 KG", choice="Boya")
+    akrilik = Paint.objects.filter(
+        name="Akrilik", location="İstanbul", weight="5 KG", choice="Boya")
+    promarin = Paint.objects.filter(
+        name="Promarin", location="İstanbul", weight="5 KG", choice="Boya")
+    underwater = Paint.objects.filter(
+        name="Under Water", location="İstanbul", weight="5 KG", choice="Boya")
+    ferforje = Paint.objects.filter(
+        name="Ferforje", location="İstanbul", weight="5 KG", choice="Boya")
+    antipas = Paint.objects.filter(
+        name="Anti Pas", location="İstanbul", weight="5 KG", choice="Boya")
+    aluminyum = Paint.objects.filter(
+        name="Aluminyum", location="İstanbul", weight="5 KG", choice="Boya")
+    resistant = Paint.objects.filter(
+        name="Isıya Dayanıklı", location="İstanbul", weight="5 KG", choice="Boya")
     context = {
-        'paints': paints,
+        'alkid': alkid,
+        'ferropox': ferropox,
+        'promega': promega,
+        'ferromel': ferromel,
+        'epoxy': epoxy,
+        'akrilik': akrilik,
+        'promarin': promarin,
+        'underwater': underwater,
+        'ferforje': ferforje,
+        'antipas': antipas,
+        'aluminyum': aluminyum,
+        'resistant': resistant,
     }
     return render(request, 'pages/kucuk-boyalar-istanbul.html', context)
 
 
-def big_paints_yalova(request):
-    paints = Paint.objects.filter(location="Yalova", weight="20 KG")
+def thinners_istanbul(request):
+    epoxy20 = Paint.objects.filter(
+        name="Epoxy", location="İstanbul", weight="20 KG", choice="Tiner")
+    epoxy12 = Paint.objects.filter(
+        name="Epoxy", location="İstanbul", weight="12 KG", choice="Tiner")
+    epoxy5 = Paint.objects.filter(
+        name="Epoxy", location="İstanbul", weight="5 KG", choice="Tiner")
+    polyurethane20 = Paint.objects.filter(
+        name="Polyurethane", location="İstanbul", weight="20 KG", choice="Tiner")
+    polyurethane12 = Paint.objects.filter(
+        name="Polyurethane", location="İstanbul", weight="12 KG", choice="Tiner")
+    polyurethane5 = Paint.objects.filter(
+        name="Polyurethane", location="İstanbul", weight="5 KG", choice="Tiner")
+    zehirli20 = Paint.objects.filter(
+        name="Zehirli", location="İstanbul", weight="20 KG", choice="Tiner")
+    zehirli12 = Paint.objects.filter(
+        name="Zehirli", location="İstanbul", weight="12 KG", choice="Tiner")
+    zehirli5 = Paint.objects.filter(
+        name="Zehirli", location="İstanbul", weight="5 KG", choice="Tiner")
+    sentetik20 = Paint.objects.filter(
+        name="Sentetik", location="İstanbul", weight="20 KG", choice="Tiner")
+    sentetik12 = Paint.objects.filter(
+        name="Sentetik", location="İstanbul", weight="12 KG", choice="Tiner")
+    sentetik5 = Paint.objects.filter(
+        name="Sentetik", location="İstanbul", weight="5 KG", choice="Tiner")
+    selulozik20 = Paint.objects.filter(
+        name="Selulozik", location="İstanbul", weight="20 KG", choice="Tiner")
+    selulozik12 = Paint.objects.filter(
+        name="Selulozik", location="İstanbul", weight="12 KG", choice="Tiner")
+    selulozik5 = Paint.objects.filter(
+        name="Selulozik", location="İstanbul", weight="5 KG", choice="Tiner")
     context = {
-        'paints': paints,
+        "epoxy20": epoxy20,
+        "epoxy12": epoxy12,
+        "epoxy5": epoxy5,
+        "polyurethane20": polyurethane20,
+        "polyurethane12": polyurethane12,
+        "polyurethane5": polyurethane5,
+        "zehirli20": zehirli20,
+        "zehirli12": zehirli12,
+        "zehirli5": zehirli5,
+        "sentetik20": sentetik20,
+        "sentetik12": sentetik12,
+        "sentetik5": sentetik5,
+        "selulozik20": selulozik20,
+        "selulozik12": selulozik12,
+        "selulozik5": selulozik5,
+    }
+    return render(request, 'pages/tinerler-istanbul.html', context)
+
+
+def big_paints_yalova(request):
+    alkid = Paint.objects.filter(
+        name="Alkid", location="Yalova", weight="20 KG", choice="Boya")
+    ferropox = Paint.objects.filter(
+        name="Ferropox", location="Yalova", weight="20 KG", choice="Boya")
+    promega = Paint.objects.filter(
+        name="Promega", location="Yalova", weight="20 KG", choice="Boya")
+    ferromel = Paint.objects.filter(
+        name="Ferromel", location="Yalova", weight="20 KG", choice="Boya")
+    epoxy = Paint.objects.filter(
+        name="Epoxy", location="Yalova", weight="20 KG", choice="Boya")
+    akrilik = Paint.objects.filter(
+        name="Akrilik", location="Yalova", weight="20 KG", choice="Boya")
+    promarin = Paint.objects.filter(
+        name="Promarin", location="Yalova", weight="20 KG", choice="Boya")
+    underwater = Paint.objects.filter(
+        name="Under Water", location="Yalova", weight="20 KG", choice="Boya")
+    ferforje = Paint.objects.filter(
+        name="Ferforje", location="Yalova", weight="20 KG", choice="Boya")
+    antipas = Paint.objects.filter(
+        name="Anti Pas", location="Yalova", weight="20 KG", choice="Boya")
+    aluminyum = Paint.objects.filter(
+        name="Aluminyum", location="Yalova", weight="20 KG", choice="Boya")
+    resistant = Paint.objects.filter(
+        name="Isıya Dayanıklı", location="Yalova", weight="20 KG", choice="Boya")
+    context = {
+        'alkid': alkid,
+        'ferropox': ferropox,
+        'promega': promega,
+        'ferromel': ferromel,
+        'epoxy': epoxy,
+        'akrilik': akrilik,
+        'promarin': promarin,
+        'underwater': underwater,
+        'ferforje': ferforje,
+        'antipas': antipas,
+        'aluminyum': aluminyum,
+        'resistant': resistant,
     }
     return render(request, 'pages/buyuk-boyalar-yalova.html', context)
 
 
 def small_paints_yalova(request):
-    paints = Paint.objects.filter(location="Yalova", weight="5 KG")
+    alkid = Paint.objects.filter(
+        name="Alkid", location="Yalova", weight="5 KG", choice="Boya")
+    ferropox = Paint.objects.filter(
+        name="Ferropox", location="Yalova", weight="5 KG", choice="Boya")
+    promega = Paint.objects.filter(
+        name="Promega", location="Yalova", weight="5 KG", choice="Boya")
+    ferromel = Paint.objects.filter(
+        name="Ferromel", location="Yalova", weight="5 KG", choice="Boya")
+    epoxy = Paint.objects.filter(
+        name="Epoxy", location="Yalova", weight="5 KG", choice="Boya")
+    akrilik = Paint.objects.filter(
+        name="Akrilik", location="Yalova", weight="5 KG", choice="Boya")
+    promarin = Paint.objects.filter(
+        name="Promarin", location="Yalova", weight="5 KG", choice="Boya")
+    underwater = Paint.objects.filter(
+        name="Under Water", location="Yalova", weight="5 KG", choice="Boya")
+    ferforje = Paint.objects.filter(
+        name="Ferforje", location="Yalova", weight="5 KG", choice="Boya")
+    antipas = Paint.objects.filter(
+        name="Anti Pas", location="Yalova", weight="5 KG", choice="Boya")
+    aluminyum = Paint.objects.filter(
+        name="Aluminyum", location="Yalova", weight="5 KG", choice="Boya")
+    resistant = Paint.objects.filter(
+        name="Isıya Dayanıklı", location="Yalova", weight="5 KG", choice="Boya")
     context = {
-        'paints': paints,
+        'alkid': alkid,
+        'ferropox': ferropox,
+        'promega': promega,
+        'ferromel': ferromel,
+        'epoxy': epoxy,
+        'akrilik': akrilik,
+        'promarin': promarin,
+        'underwater': underwater,
+        'ferforje': ferforje,
+        'antipas': antipas,
+        'aluminyum': aluminyum,
+        'resistant': resistant,
     }
     return render(request, 'pages/kucuk-boyalar-yalova.html', context)
 
 
-def promega_yalova(request):
-    paints = Paint.objects.filter(
-        name="Promega", location="Yalova", weight="20 KG")
-    paints2 = Paint.objects.filter(
-        name="Promega", location="Yalova", weight="5 KG")
+def thinners_yalova(request):
+    epoxy20 = Paint.objects.filter(
+        name="Epoxy", location="Yalova", weight="20 KG", choice="Tiner")
+    epoxy12 = Paint.objects.filter(
+        name="Epoxy", location="Yalova", weight="12 KG", choice="Tiner")
+    epoxy5 = Paint.objects.filter(
+        name="Epoxy", location="Yalova", weight="5 KG", choice="Tiner")
+    polyurethane20 = Paint.objects.filter(
+        name="Polyurethane", location="Yalova", weight="20 KG", choice="Tiner")
+    polyurethane12 = Paint.objects.filter(
+        name="Polyurethane", location="Yalova", weight="12 KG", choice="Tiner")
+    polyurethane5 = Paint.objects.filter(
+        name="Polyurethane", location="Yalova", weight="5 KG", choice="Tiner")
+    zehirli20 = Paint.objects.filter(
+        name="Zehirli", location="Yalova", weight="20 KG", choice="Tiner")
+    zehirli12 = Paint.objects.filter(
+        name="Zehirli", location="Yalova", weight="12 KG", choice="Tiner")
+    zehirli5 = Paint.objects.filter(
+        name="Zehirli", location="Yalova", weight="5 KG", choice="Tiner")
+    sentetik20 = Paint.objects.filter(
+        name="Sentetik", location="Yalova", weight="20 KG", choice="Tiner")
+    sentetik12 = Paint.objects.filter(
+        name="Sentetik", location="Yalova", weight="12 KG", choice="Tiner")
+    sentetik5 = Paint.objects.filter(
+        name="Sentetik", location="Yalova", weight="5 KG", choice="Tiner")
+    selulozik20 = Paint.objects.filter(
+        name="Selulozik", location="Yalova", weight="20 KG", choice="Tiner")
+    selulozik12 = Paint.objects.filter(
+        name="Selulozik", location="Yalova", weight="12 KG", choice="Tiner")
+    selulozik5 = Paint.objects.filter(
+        name="Selulozik", location="Yalova", weight="5 KG", choice="Tiner")
     context = {
-        'paints': paints,
-        'paints2': paints2,
+        "epoxy20": epoxy20,
+        "epoxy12": epoxy12,
+        "epoxy5": epoxy5,
+        "polyurethane20": polyurethane20,
+        "polyurethane12": polyurethane12,
+        "polyurethane5": polyurethane5,
+        "zehirli20": zehirli20,
+        "zehirli12": zehirli12,
+        "zehirli5": zehirli5,
+        "sentetik20": sentetik20,
+        "sentetik12": sentetik12,
+        "sentetik5": sentetik5,
+        "selulozik20": selulozik20,
+        "selulozik12": selulozik12,
+        "selulozik5": selulozik5,
     }
-    return render(request, 'pages/promega-yalova.html', context)
+    return render(request, 'pages/tinerler-yalova.html', context)
 
 
 def addPaint(request):
